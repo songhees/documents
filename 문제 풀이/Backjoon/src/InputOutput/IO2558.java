6a05a792ef09fc6bb8e26bf0f5bd3351c41aa1c1
songhees <osh4880@gmail.com>
2022-01-11 16:43:15 +0900

    resolved:#2

---
+++ 문제 풀이/Backjoon/src/InputOutput/IO2558.java
@@ -0,0 +1,16 @@
+package InputOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class IO2558 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+	
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.next() + sc.next());
+	}
+}
