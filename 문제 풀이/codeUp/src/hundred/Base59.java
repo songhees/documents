c8d397889fc9743f3192be16aefd57a0a5ddc322
songhees <osh4880@gmail.com>
2022-03-06 23:58:03 +0900

    [ADD] codeup풀이

--- 문제 풀이/codeUp/src/hundred/Base59.java
+++ 문제 풀이/codeUp/src/hundred/Base59.java
@@ -1,5 +1,13 @@
 package hundred;
 
-public class Base59 {
+import java.io.*;
 
+public class Base59 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(bf.readLine());
+		// 2 = 0010 -> ~2 = 1101 가 왜 -3일까??  1의 보수??
+		System.out.print(~num);
+	}
 }
