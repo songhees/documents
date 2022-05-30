02f9ec7aeb784f430eb336f84e8ade118e11b343
songhees <osh4880@gmail.com>
2022-05-30 18:44:01 +0900

    [ADD] thymeleaf 연습

--- intellij/ex1/src/main/java/com/zerock/ex1/controller/SampleController.java
+++ intellij/ex1/src/main/java/com/zerock/ex1/controller/SampleController.java
@@ -1,14 +1,21 @@
 package com.zerock.ex1.controller;
 
-import lombok.extern.log4j.Log4j;
+import com.zerock.ex1.dto.SampleDTO;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
-@Log4j
+@Log4j2
 @RequestMapping("/sample")
 public class SampleController {
 
@@ -16,4 +23,52 @@ public class SampleController {
     public @ResponseBody String[] hello() {
         return new String[]{"Hello", "World"};
     }
+
+    @GetMapping("/ex1")
+    public void ex1() {
+        log.info("ex1............");
+    }
+
+    @GetMapping({"/ex2", "/exLink"})
+    public void ex2(Model model) {
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.." + i)
+                    .last("Last.." + i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline.............");
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("First..100")
+                .last("Last..100")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+        log.info("ex3");
+    }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
+    public void exLayout1() {
+        log.info("exLayout................");
+    }
+
+
 }
