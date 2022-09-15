5d51c163048c365bb80c49298694e22f10338eae
songhees <osh4880@gmail.com>
2022-09-15 18:53:03 +0900

    [ADD] naver login 기능 추가 중

--- AWS/awsProject/src/main/java/com/awsProject/config/auth/SecurityConfig.java
+++ AWS/awsProject/src/main/java/com/awsProject/config/auth/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
